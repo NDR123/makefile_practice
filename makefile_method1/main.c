75430f78f40c65a1b9c823b1fc386ad8c03e6619
123NDR <divyanadella98@gmail.com>
2023-05-15 09:02:27 +0530

    makefile practice

---
+++ makefile_method1/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int add(int , int);
+int sub(int , int);
+int mul(int , int);
+int main()
+{
+	printf("sum = %d\n", add(10,30) );
+	printf("mul = %d\n", mul(10,30) );
+	printf("sub= %d\n", sub(10,30) );
+}
+
+
+
